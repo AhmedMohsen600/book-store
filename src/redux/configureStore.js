529593852db13506eaebe566fa25edfd01ed7d10
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2022-12-12 21:41:28 +0200

    fix configureStore error

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,10 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
-const rootReducers = combineReducers({
-  booksReducer,
-  categoriesReducer,
+export const store = configureStore({
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
 });
-
-export const store = configureStore(rootReducers);
